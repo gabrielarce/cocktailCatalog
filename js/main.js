d65ad01f8de3018f0dbf8bb9611dc7da27dc25c3
Gabe <gabrielarce3@gmail.com>
2022-04-27 23:14:58 -0700

    js functions added. working on css versions

--- js/main.js
+++ js/main.js
@@ -5,20 +5,19 @@ navLinks.forEach((element) => {
     element.addEventListener("click", soFetch)
 })
 
-// navLinks.forEach(element=>{
-//   let letter = element.innerHTML;
-//   element.addEventListener("click", console.log(letter))
-// })
 
-
+let currentDrinks = {};
+let ul = document.getElementById("drinksList");
 
 function soFetch() {
     let letter = this.innerHTML
     fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${letter}`)
         .then(res => res.json())
         .then(data => {
-            clearDrinks()
+            currentDrinks = data
+            clearDrinksHTML()
             drinksByLetter(data)
+
         })
         .catch(err => {
             console.log(`error ${err}`)
@@ -26,18 +25,71 @@ function soFetch() {
 }
 
 function drinksByLetter(data) {
-    console.log(data)
-    console.log(data.drinks)
-    let letterDrinks = data.drinks
-    letterDrinks.forEach((element) => {
-        let ul = document.getElementById("list");
+    let drinks = data.drinks
+    drinks.forEach((element, index) => {
+        let ul = document.querySelector("#drinksList");
         let li = document.createElement("li");
+        li.setAttribute("data-icon", "🍹")
+        li.addEventListener("click", function(e) {
+            renderDrinkInfo(drinks[index])
+        })
         ul.appendChild(li);
         li.innerHTML = element.strDrink;
-        console.log(element.strDrink)
-    })
+    });
+
 }
 
-function clearDrinks() {
-    document.getElementById("list").innerHTML = ""
+function clearDrinksHTML() {
+    document.getElementById("drinksList").innerHTML = ""
+}
+
+
+function renderDrinkInfo(data) {
+    let h2 = document.querySelector("h2")
+    let img = document.querySelector("img")
+    let p = document.querySelector("p")
+
+    h2.innerHTML = data.strDrink;
+    img.setAttribute("src", data.strDrinkThumb)
+    p.innerHTML = data.strInstructions
+    renderIngredients(data)
+}
+
+function renderIngredients(data) {
+    //get only truthy values ingredients
+    document.querySelector(".ingredients").innerHTML = ""
+    document.querySelector(".measurements").innerHTML = ""
+    let indgredients = [];
+    for (let property in data) {
+
+        if (property.includes("strIngredient") && data[property]) {
+            indgredients.push(data[property])
+            console.log(data[property])
+        }
+    }
+    //add ingredients to DOM
+    for (let i = 0; i < indgredients.length; i++) {
+        let ingredientsList = document.querySelector(".ingredients")
+        let li = document.createElement("li")
+        li.innerHTML = indgredients[i]
+        ingredientsList.appendChild(li)
+    }
+
+    //get only truthy values for measurements
+    let measurements = [];
+    for (let property in data) {
+
+        if (property.includes("strMeasure") && data[property]) {
+            measurements.push(data[property])
+            console.log(data[property])
+        }
+    }
+
+    //add ingredients to DOM
+    for (let i = 0; i < indgredients.length; i++) {
+        let measurementList = document.querySelector(".measurements")
+        let li = document.createElement("li")
+        li.innerHTML = measurements[i]
+        measurementList.appendChild(li)
+    }
 }
